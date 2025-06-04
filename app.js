31af14ea7606af16f4a3a4a8b151125247ae3849
Kai Kameyama <1203kk06@gmail.com>
2025-06-04 20:28:43 +0800

    added login/logout with jtw tokes

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const express = require('express');
+const cors = require('cors');
+const cookieParser = require("cookie-parser");
 
 const homeRoutes = require("./routes/HomeRoutes");
-const userRoutes = require("./routes/UserRoutes")
+const userRoutes = require("./routes/UserRoutes");
 
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cookieParser());
+
+app.use(cors({
+    origin: `http://localhost:${port}`, 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    credentials: true
+  }));
+
 
 app.use("/api/v1/home", homeRoutes);
 app.use("/api/v1/user", userRoutes);
---
+++ controllers/UserController.js
@@ -0,0 +1,78 @@
+const prisma = require("../config/prismaConfig");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
+require('dotenv').config();
+
+async function signUp(req, res) {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+        return res.status(400).json({ error: "Required fields not found." })
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    try {
+        await prisma.user.create({
+            data: {
+                name,
+                email,
+                password: hashedPassword
+            }
+        })
+        res.status(200).json({ message: "User created!" });
+    } catch (err) {
+        console.error("There was a error making the user: ", err);
+        res.status(500).json({ error: "Something went wrong" });
+    }
+}
+
+async function signIn(req, res) {
+    const { email, password } = req.body;
+    try {
+        const user = await prisma.user.findUnique({
+            where: { email }
+        })
+
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        const validPass = await bcrypt.compare(password, user.password);
+        if (!validPass) {
+            return res.status(401).json({ error: "Incorrect password" });
+        }
+
+        const token = jwt.sign(
+            { id: user.id, name: user.name },
+            process.env.JWT_SECRET,
+            { expiresIn: "6h" }
+        );
+
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+            maxAge: 3600000 * 6
+        });
+
+        return res.status(200).json({ message: "User signed in successfully!" });
+    } catch (err) {
+        console.error("Could not create user: ", err);
+        return res.status(500).json({ error: "Internal server error." });
+    }
+}
+
+function signOut(req, res) {
+    res.clearCookie("token", {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "Strict",
+    });
+    res.json({ message: "Logged out" });
+};
+
+module.exports = {
+    signUp,
+    signIn,
+    signOut
+}
